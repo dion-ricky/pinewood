c7c1446a48cd01e43d3463936fb57600ddd11d0b
Dion Ricky Saputra <dionrickysptr@gmail.com>
2022-10-01 08:13:46 +0700

    Logged to stdout

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import logging
 from datetime import datetime
 
 import functions_framework
 
 @functions_framework.cloud_event
 def main(cloud_event):
-    logger = logging.getLogger('main')
-    logger.info(f"Executed at {datetime.now()}")
+    print(f"Executed at {datetime.now()}")
